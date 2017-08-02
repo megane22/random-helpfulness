336e07fa00413ccc9ed8b421a182a9c45cd20a8c
mooghin <mooghin@gmail.com>
2017-08-02 07:57:15 -0700

    Reclaiming my time

--- break_time.py
+++ break_time.py
@@ -6,7 +6,7 @@ break_count = 0
 
 print("This program started on " + time.ctime())
 while (break_count < total_breaks):
-    time.sleep(10)
-    webbrowser.open("https://www.youtube.com/watch?v=iIPH8LFYFRk")
+    time.sleep(60)
+    webbrowser.open("https://www.youtube.com/watch?v=tqCZzs7UXIo")
     print("Took a break at " + time.ctime())
     break_count = break_count + 1
